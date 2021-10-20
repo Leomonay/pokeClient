a0b83044345d86a0703d35f2e6b0aa7abc286c9f
leomonay <leonardomonay@gmail.com>
2021-10-20 13:19:06 -0300

    page buttons size

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import './App.css';
 
 function App() {
   return (
-    <div>
       <Router>
       <Switch>
             <Route exact path={'/'} component={Landing} hideMenuBar={true}></Route>
@@ -25,7 +24,6 @@ function App() {
         </Layout>
         </Switch>
       </Router>
-    </div>
   );
 }
 
