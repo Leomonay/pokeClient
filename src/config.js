695755e7002a3b965756d95f6d0a44aec64ae3c3
leomonay <leonardomonay@gmail.com>
2021-10-22 11:15:39 -0300

    server link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const appConfig={
-    // host: 'https://leomonay-pokedexserver.herokuapp.com',
-    host: 'http://localhost:3003',
+    host: 'https://leomonay-pokedexserver.herokuapp.com',
+    // host: 'http://localhost:3003',
 }
 export default appConfig
